9d9938f141457b3e5f2fb4f13316e18b12d6e171
Leon <31255961+leon-biju@users.noreply.github.com>
2024-08-19 09:23:03 +0100

    Adds to-do-list and functionality to add items

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import *
 # Create your views here.
 
+from .models import *
+
 def index(request):
-  return HttpResponse("Hello World")
+  tasks = Task.objects.all()
+  form = TaskForm()
+
+  if request.method == 'POST':
+    form = TaskForm(request.POST)
+    if form.is_valid():
+      form.save()
+    return redirect('/')
+
+  return render(request, "tasks/list.html", {"tasks": tasks, "form": form})
